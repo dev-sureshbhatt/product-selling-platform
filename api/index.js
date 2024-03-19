c6e4755901729d0387afd810888b9e367007ffb9
dev-sureshbhatt <dev.sureshbhatt@gmail.com>
2024-03-19 13:59:22 +0530

    Mongoose setup and  User model creation

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const app = express()
+const MONGO_STRING = process.env.MONGO_STRING
 
 app.listen('4000', ()=>{
     console.log("app listening")
 })
+
+
+mongoose.connect(MONGO_STRING).then(()=>{}).catch(()=>{console.log("something went wrong connecting MONGOOSE")})
